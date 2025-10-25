6af3b9d0bbe2715795d9126cc5e1f2b7420902d7
Daria Tuzova <darituzova@yandex.ru>
2025-10-25 18:50:32 +0300

    create a test window

--- waves.py
+++ waves.py
@@ -1,1 +1,16 @@
-PyQt5.QtWidgets import QApplication, QWidget, from PyQt5.QtGui import QPainter
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPainter
+
+class Waves(QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        self.setGeometry(500, 600, 1000, 700)
+
+app = QApplication(sys.argv)
+
+window_waves = Waves()
+window_waves.show()
+
+sys.exit(app.exec_())

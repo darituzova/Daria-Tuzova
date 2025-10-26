6c5bc004884e70444b68505813fd1d2a2b54bd42
Daria Tuzova <darituzova@yandex.ru>
2025-10-26 12:05:28 +0300

    base pygame

--- waves_pygame.py
+++ waves_pygame.py
@@ -1,1 +1,20 @@
 import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+    
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()

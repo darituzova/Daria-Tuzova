5d70655f526787b60094618f04a347e6e1cfe5a4
Daria Tuzova <darituzova@yandex.ru>
2025-10-26 13:52:15 +0300

    add automatic vetrical shifts

--- waves_pygame.py
+++ waves_pygame.py
@@ -3,6 +3,25 @@ import sys
 import numpy as np
 import math
 
+def set_shift_vertical_to_sinusoids(sinusoids, screen_height):
+    total_amplitude = sum(2 * sinusoid.amplitude for sinusoid in sinusoids)
+    
+    spacing = 20
+    all_spacing = spacing * (len(sinusoids) - 1)
+    avail_height = screen_height - all_spacing
+    need_height = total_amplitude + all_spacing
+    
+    if need_height > avail_height:
+        scale = avail_height / need_height
+        for sinusoid in sinusoids:
+            sinusoid.amplitude *= scale
+        total_amplitude *= scale
+
+    current_y = 0
+    
+    for sinusoid in sinusoids:
+        sinusoid.vertical_shift = current_y + sinusoid.amplitude
+        current_y = sinusoid.vertical_shift + sinusoid.amplitude + spacing
 class Sinusoid:
     def __init__(self, amplitude, frequency, speed, color, line_width):
         self.amplitude = amplitude
@@ -11,14 +30,13 @@ class Sinusoid:
         self.color = color
         self.line_width = line_width
         
+        self.vertical_shift = 0
         self.t = 0
         self.points = []
     
     def create_sinusoid(self, screen_width, screen_height):
-        shift_vertical = screen_height // 2
-        
         x = np.arange(0, screen_width, 2)
-        y = self.amplitude * np.sin(self.frequency * x + self.speed * self.t) + shift_vertical
+        y = self.amplitude * np.sin(self.frequency * x + self.speed * self.t) + self.vertical_shift
         
         points = np.stack((x, y.astype(np.int32)), axis=1)
         
@@ -36,7 +54,9 @@ pygame.init()
 
 screen = pygame.display.set_mode()
 
-sinusoid = Sinusoid(100, 0.1, 0.1, (0, 255, 0), 1)
+sinusoids = [Sinusoid(200, 0.1, 0.1, (0, 255, 0), 2), Sinusoid(200, 0.02, 0.04, (255, 0, 0), 2), Sinusoid(100, 0.5, 0.5, (0, 0, 255), 2)]
+
+set_shift_vertical_to_sinusoids(sinusoids, screen.get_height())
 
 running = True
 while running:
@@ -47,8 +67,9 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
     
-    screen.fill((0, 0, 0)) 
-    sinusoid.draw_sinusoid(screen)
+    screen.fill((0, 0, 0))
+    for sinusoid in sinusoids: 
+        sinusoid.draw_sinusoid(screen)
     
     pygame.display.flip()
 

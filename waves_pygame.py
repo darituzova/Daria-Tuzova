196cdfe870c6a277532a39468399b1cca49f193d
Daria Tuzova <darituzova@yandex.ru>
2025-10-26 16:35:02 +0300

    default circles, paddings, as example

--- waves_pygame.py
+++ waves_pygame.py
@@ -3,20 +3,20 @@ import sys
 import numpy as np
 
 
-def set_shift_vertical_to_sinusoids(sinusoids, screen_height, spacing=20):
+def set_shift_vertical_to_sinusoids(sinusoids, screen_height, spacing=20, padding=10):
     total_amplitude = sum(2 * sinusoid.amplitude for sinusoid in sinusoids)
     
     all_spacing = spacing * (len(sinusoids) - 1)
-    avail_height = screen_height - all_spacing
+    avail_height = screen_height - 2 * padding
     need_height = total_amplitude + all_spacing
     
-    if need_height <= screen_height:
+    if need_height <= avail_height:
         center = screen_height // 2
         current_y = center - need_height // 2
         flag_scale = False
     else:
-        scale = avail_height / need_height
-        current_y = 0
+        scale = (avail_height + all_spacing) / need_height
+        current_y = padding
         flag_scale = True
     
     for sinusoid in sinusoids:
@@ -26,7 +26,7 @@ def set_shift_vertical_to_sinusoids(sinusoids, screen_height, spacing=20):
         current_y = sinusoid.vertical_shift + sinusoid.amplitude + spacing
         
 class Sinusoid:
-    def __init__(self, amplitude=50, frequency=0.2, speed=0.1, color=(128, 128, 128), line_width=1, step=1, time_step=0.05):
+    def __init__(self, amplitude=50, frequency=0.2, speed=0.1, color=(255, 0, 0), line_width=3, step=1, time_step=0.05):
         self.amplitude = amplitude
         self.frequency = frequency
         self.speed = speed
@@ -56,7 +56,7 @@ class Sinusoid:
         
         self.update_circles(screen.get_width())
         for circle in self.circles:
-            circle.draw_circle(screen)
+            circle.draw_circle(screen, edge_color=self.color)
         
     def attach_circle(self, circle, x=0):
         circle.x = x
@@ -69,7 +69,7 @@ class Sinusoid:
             circle.y = int(self.amplitude * np.sin(self.frequency * circle.x + self.speed * self.t) + self.vertical_shift)
 
 class Circle:
-    def __init__(self, sinusoid, weight, volume, radius, color, x=0):
+    def __init__(self, sinusoid, weight=50, volume=60, radius=15, color=(0, 0, 255), x=0):
         self.sinusoid = sinusoid
         self.weight = weight
         self.volume = volume
@@ -83,19 +83,18 @@ class Circle:
         
         sinusoid.attach_circle(self, x)
     
-    def draw_circle(self, screen):
+    def draw_circle(self, screen, edge_color):
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
-        pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.radius, 2)
+        pygame.draw.circle(screen, edge_color, (self.x, self.y), self.radius, 2)
 
 
 pygame.init()
 
 screen = pygame.display.set_mode()
-
-sinusoids = [Sinusoid(), Sinusoid(200, 0.02, 0.04, (255, 0, 0), 3), Sinusoid(100, 0.07, 0.05, (0, 0, 255), 2)]
+sinusoids = [Sinusoid(amplitude=100, frequency=0.07, speed=0.07), Sinusoid(amplitude=100, frequency=0.05, speed=0.05), Sinusoid(amplitude=100, frequency=0.03, speed=0.03), Sinusoid(amplitude=100, frequency=0.01, speed=0.1)]
 set_shift_vertical_to_sinusoids(sinusoids, screen.get_height())
 
-circles = [Circle(sinusoids[0], 50, 60, 10, (255, 255, 0)), Circle(sinusoids[1], 80, 60, 20, (255, 0, 0)), Circle(sinusoids[2], 30, 60, 8, (0, 255, 0))]
+circles = [Circle(sinusoids[0]), Circle(sinusoids[1]), Circle(sinusoids[2]), Circle(sinusoids[3])]
 
 running = True
 while running:
@@ -106,7 +105,7 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
     
-    screen.fill((135, 206, 235))
+    screen.fill((239, 238, 238))
     for sinusoid in sinusoids: 
         sinusoid.draw(screen)
 
